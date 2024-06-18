06fa66322d6bd9387b5d7ac7fa4af7f2f5df4204
Tomás <troldanborjas@mail.austral.edu.ar>
2024-06-18 18:56:57 -0300

    Minor changes to add a BreakCondition

--- src/main/java/org/example/amazonwordcloud/words/FrequencyRanker.java
+++ src/main/java/org/example/amazonwordcloud/words/FrequencyRanker.java
@@ -11,10 +11,12 @@ public class FrequencyRanker implements WordRanker {
 
     private final WordFilter filter;
     private final Set<Character> delimiters;
+    private final BreakCondition breakCondition;
 
-    public FrequencyRanker(WordFilter filter, Set<Character> delimiters) {
+    public FrequencyRanker(WordFilter filter, Set<Character> delimiters, BreakCondition breakCondition) {
         this.filter = filter;
         this.delimiters = delimiters;
+        this.breakCondition = breakCondition;
     }
 
     @Override
@@ -26,20 +28,26 @@ public class FrequencyRanker implements WordRanker {
         // Instead, we will create these substrings once we are
         // interested in the word in question.
 
-        for (int start = 0, end = 0; end < text.length(); end++) {
+        int length = text.length();
+        for (int start = 0, end = 0; end < length; end++) {
             //TODO: last word is ignored
             if (delimiters.contains(text.charAt(end))) {
                 if (!filter.isAvoidable(text, start, end)) {
-                    countWordUp(text.substring(start, end).toLowerCase(), result);
+                    String word = text.substring(start, end).toLowerCase();
+                    int frequency = result.getOrDefault(word, 0) + 1;
+                    result.put(word, frequency);
+
+                    if (breakCondition.isMet(frequency, start, end, length)) break;
                 }
                 start = end + 1;
             }
+
         }
 
         return result;
     }
 
     private static void countWordUp(String word, Map<String, Integer> result) {
-        result.put(word, result.getOrDefault(word, 0) + 1);
+
     }
 }
